c3f1db8e280f775cf4b574ead95481af7e675c28
Toccator <18370608156@163.com>
2020-12-28 09:50:40 +0800

    test3

--- src/main/java/FunctionInterfaceTest/Useing/Function.java
+++ src/main/java/FunctionInterfaceTest/Useing/Function.java
@@ -7,6 +7,8 @@ public interface Function<T, R> {
 
     R apply(T t);
 
+
+
     default <V> Function<V, R> compose(Function<? super V, ? extends T> before) {
         Objects.requireNonNull(before);
         return (V v) -> apply(before.apply(v));
